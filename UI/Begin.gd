1da3f6163eecab3ea941529493304c002e030ca3
tglm <mnillx@outlook.com>
2023-12-14 16:48:30 +0800

    结束

--- UI/Begin.gd
+++ UI/Begin.gd
@@ -12,3 +12,21 @@ func _on_exit_button_pressed() -> void:
 	
 func _ready() -> void:
 	$CharacterBody2D.position = $Marker2D.position
+
+
+func _on_start_zone_area_entered(area: Area2D) -> void:
+	$ColorRect/StartZone/StartTimer.start()
+
+
+func _on_start_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://Main.tscn")
+
+
+
+func _on_record_zone_area_entered(area: Area2D) -> void:
+	$ColorRect/RecordZone/RecordTimer.start()
+
+
+#todo
+func _on_record_timer_timeout() -> void:
+	pass
